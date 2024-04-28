d9e5e90c39fe2332f537923f00802962b60ed9b1
Rafael Ziani de Carvalho <rafael.ziani1@gmail.com>
2024-04-28 16:28:10 -0300

    todoo

--- Codes 1-5/1 -/1 - Analisando Sentimentos.py
+++ Codes 1-5/1 -/1 - Analisando Sentimentos.py
@@ -20,14 +20,14 @@ def analyze_sentiment():
     count_positivo = sum(palavra in positivas for palavra in palavras)
     count_negativo = sum(palavra in negativas for palavra in palavras)
     
-    # TODO: Conte quantas palavras neutras estão presentes no comentáriopara classificação.  
+    # TOD0: Conte quantas palavras neutras estão presentes no comentáriopara classificação.  
     count_neutro = sum(palavra in neutras for palavra in palavras)
     
     # Verifica se há mais palavras positivas do que negativas no comentário e se não há palavras neutras.
     # Se essa condição for verdadeira, o comentário é considerado positivo.
     if count_positivo > count_negativo and count_neutro == 0:
         return "Positivo"
-    # TODO: Complete a codição para determinar o sentimento com base na contagem de palavras
+    # TOD0: Complete a codição para determinar o sentimento com base na contagem de palavras
     
     # Verifica se há mais palavras negativas do que positivas no comentário e se não há palavras neutras. Se essa condição for verdadeira, o comentário é considerado negativo.
     if count_negativo > count_positivo and count_neutro == 0:
